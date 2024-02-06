4c2f4ee3313bd546e0425e79da75a9e565216b31
GasparMon <imoncivaez@gmail.com>
2024-02-06 19:06:13 +0100

    modelos

---
+++ server/database/models/Course.js
@@ -0,0 +1,84 @@
+const { Schema, model } = require(`moongose`);
+
+const CourseSchema = new Schema({
+  lenguage: {
+    type: String,
+    require: true,
+    unique: false,
+    enum: ["English, French, German, Italian, Dutch, Portuguese"],
+  },
+
+  level: {
+    type: String,
+    require: true,
+    unique: false,
+    enum: ["Beginer, Intermediate, Advanced"],
+  },
+
+  price: {
+    type: Number,
+    require: true,
+    unique: false,
+    min: 25,
+    max: 300,
+  },
+
+  duration: {
+    type: String,
+    requiere: true,
+    unique: false,
+    enum: ["1 Month , 2 Months, 3 Months, 4 Months"],
+  },
+
+  schedule: {
+    type: String,
+    require: true,
+    unique: false,
+    enum: [
+      "On Weekends, During the week, Monday-Wednesday-Friday, Tuesday-Thursday",
+    ],
+  },
+
+  start_time: {
+    type: String,
+    require: true,
+    unique: false,
+  },
+
+  finish_time: {
+    type: String,
+    require: true,
+    unique: false,
+  },
+
+  location: {
+    type: String,
+    require: true,
+    unique: false,
+  },
+
+  image: {
+    type: String,
+    require: true,
+    unique: false,
+  },
+  student_course: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+
+  company_course: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+
+  review_course:{
+    type: Schema.Types.ObjectId,
+    ref: "Review",
+    require: true,
+  }
+});
+
+module.exports = model("Course", CourseSchema);
---
+++ server/database/models/User.js
@@ -0,0 +1,77 @@
+const { Schema, model } = require(`mongoose`);
+const validator = require(`validator`)
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    require: false,
+    unique: false,
+    minlength: 3,
+    maxlength: 30
+  },
+
+  lastname:{
+    type: String,
+    require: false,
+    unique: false,
+    minlength: 3,
+    maxlength: 30
+  },
+
+  age:{
+    type:Number,
+    require: false,
+    unique: false,
+    min: 8,
+    max: 99
+  },
+
+  company_name: {
+    type: String,
+    require: false,
+    unique: true,
+    minlength: 3,
+    maxlength: 30
+  },
+
+  email:{
+    type:String,
+    require: true,
+    unique: true,
+    lowcase: true,
+    validate:{
+        validator: (value) => validator.isEmail(value),
+        message: `Please, Insert a valid Email`
+    }
+  },
+
+  password:{
+    type:String,
+    require: true,
+    unique: false,
+    minlength: 6,
+    maxlength: 15,
+    match:[
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+        `Please, Insert a valid Password`
+    ]
+  },
+  user_course:{
+    type: Schema.Types.ObjectId,
+    ref:"Course",
+    require: false,
+  },
+  user_reviews:{
+    type: Schema.Types.ObjectId,
+    ref: "Reviews",
+    require: false,
+  },
+  user_payment:{
+    type: Schema.Types.ObjectId,
+    ref: "Payment",
+    require: false
+  }
+
+});
+
+module.exports = model("User", UserSchema);
