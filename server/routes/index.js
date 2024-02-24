f701587a8d0c2a3ae9c5934408bab46c2bcc808d
tomaspon <tomaspon11@gmail.com>
2024-02-24 01:36:07 -0300

    features(server/handlers/routes - REVIEW corrections )

--- server/routes/index.js
+++ server/routes/index.js
@@ -59,7 +59,7 @@ routes.get("/getCoursePayment/:id", getPaymentCourse);
 routes.delete("/deleteReview/:id", deleteReview);
 routes.put("/putDeleteCourse/:id", deleteCourse);
 routes.put("/deleteUser/:id", deleteUser);
-routes.put("/putReview", putReview);
+routes.put("/putReview/:reviewId", putReview);
 routes.put("/putUser", putUser);
 routes.put("/putCourse", putCourse);
 routes.put("/putPayment", putPayment);
@@ -71,9 +71,9 @@ routes.get("/getAllPayments", getAllPayments);
 routes.get("/getAllReviews", getAllReviews);
 routes.get("/getCourseFilters", getCourseFilters);
 routes.post("/createPreference", createPreference);
-routes.get("/getCart/:id", getCart)
-routes.put("/addCartProduct", addCartProduct)
-routes.put("/deleteCartProduct", deleteCartProduct)
-routes.put("/closeCart/:id", closeCart)
+routes.get("/getCart/:id", getCart);
+routes.put("/addCartProduct", addCartProduct);
+routes.put("/deleteCartProduct", deleteCartProduct);
+routes.put("/closeCart/:id", closeCart);
 
 module.exports = routes;
